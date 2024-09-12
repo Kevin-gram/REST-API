f5f4f3a737bf209fdcb171f20ec5b94ccf107eef
Kevin-gram <k.nyiringan@alustudent.com>
2024-09-12 18:50:41 +0200

    chore: connecting the server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/subsribers");
+mongoose.connect(
+  "mongodb+srv://nyiringangokevin4:nyiringangokevin4@node.vbkfa.mongodb.net/?retryWrites=true&w=majority&appName=Node"
+);
 app.listen(3000, () => console.log("server started "));
